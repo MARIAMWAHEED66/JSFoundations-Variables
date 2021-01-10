ad96eccb0ed52ff6b30f463d232b8b3b65b1f284
Sabreen Bucheerei <m_7maid@hotmail.com>
2021-01-10 14:22:35 +0300

    i did it yaay

--- story.js
+++ story.js
@@ -1,9 +1,10 @@
 
-
-let name = "Mshary";
-let age = 25;
-let hobby = "Meditation";
-
+let Name = "Mariam";
+let age = 23;
+let hobby = "coding";
 
 
-console.log('Mr./Mrs. ${name}, in 33 years, you will be ${age+33} years old. By then you will have decades of ${hobby} practice!`)
+
+console.log(`Mr./Mrs. ${Name}, in 33 years, you will be ${age+33} years old. By then you will have decades of ${hobby} practice!`);
+
+
